acb00837109fbcddc9c4d6bff2296b5242ff72ee
xDamneDx <xdamnedx@gmail.com>
2022-08-30 22:10:57 +0300

    Completed exercise 10/01

---
+++ 10_01.js
@@ -0,0 +1,8 @@
+// Напиши функцию getStringLength, которая принимает строку input 
+// и возвращает ее длину (количество символов в ней).
+
+function getStringLength(input) {
+    return input.length;
+}
+
+console.log(getStringLength('Some String For Testing')); // 
